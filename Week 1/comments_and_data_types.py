924e6fa3e060f0920920534f125e6a6f2c847fd6
Jonathan Karr <jka83321@gmail.com>
2020-11-30 02:17:29 -0500

    init

---
+++ Week 1/comments_and_data_types.py
@@ -0,0 +1,12 @@
+# This is a comment. The python Interpreter doesn't read this
+# There are 4 main types of data types string, int, float, boolean
+
+#the default for input is a string
+name = input ("What is your name: ")
+
+# in order to make an input another data type you must convert it
+age = int(input("What is your age: ")) #You can have an inline comment like this
+
+# The print statement can only take in strings. Here are 2 options
+print("Hello " + name + " you are " + str(age))
+print("Hello", name, "you are", age)
---
+++ Week 1/functions_to_modify_strings.py
@@ -0,0 +1,5 @@
+age= int(input("What is your age: "))
+first_name = input("What is your first name: ")
+last_name= input("What is youe last name: ")
+age= int(input("What is your age: "))
+print("Hello " + first_name.capitalize() + " " + last_name.capitalize() + " you are" + " " + str(age))
