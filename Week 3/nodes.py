ff711f7bba25edac0e2fcba880c31ed7043310c3
Jonathan Karr <jka83321@gmail.com>
2020-12-16 00:19:17 -0500

    First Part of Google Interview Question

---
+++ Week 3/nodes.py
@@ -0,0 +1,27 @@
+"""First part of Sample Google Interview Question
+Find the total number of nodes in this tree
+       1
+     /   \\
+    2      3
+   / \\    / \\
+  4   5   6   7
+ /\\
+8  9                       """
+
+#before we right code let's think about how the algorithim works
+# let's just go from the node 9 first
+number = int(input("enter a positve integer: "))
+nodes=0
+while (2**(nodes+1) <= number):
+    nodes+=1
+print(nodes)
+
+#now that we can calculate the number of nodes from a value, we can find the total nodes by using another while statement
+total_nodes =0
+while (number>0):
+  nodes=0
+  while (2**(nodes+1) <= number):
+    nodes+=1
+  number-=1
+  total_nodes += nodes
+print(total_nodes)
