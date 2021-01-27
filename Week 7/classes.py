67a95a5c892b64a58c708421aa94b56318bd2bba
Jonathan Karr <jka83321@gmail.com>
2021-01-27 16:35:05 -0500

    Week 7

---
+++ Week 7/classes.py
@@ -0,0 +1,7 @@
+class student:
+    def __init__(self,name,grade):
+        self.name = name
+        self.grade = grade
+student1 = student("Eli",12)
+student2 = student("Emilo",11)
+print(student1.name)
