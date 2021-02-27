3af5db596a5bdc27953afb00b3766d04de291e72
Jonathan Karr <jka83321@gmail.com>
2021-02-26 23:05:09 -0500

    named tuples

--- More_Content_After_The_Course/named_tuple.py
+++ More_Content_After_The_Course/named_tuple.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
-color = namedtuple('color',('red','blue','green'))
+color = namedtuple('color',('red','green','blue'))
 first_color = color(232,34,95)
-second_color = color(red=22,green=23,blue=233)
+second_color = color(red=22,blue=23,green=233)
 print(first_color[0])
 print(first_color.blue)
 print(first_color)
