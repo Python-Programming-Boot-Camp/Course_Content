471ae0a382da1c33670c418e6dd9541f835cea13
Jonathan Karr <jka83321@gmail.com>
2021-04-05 23:29:07 -0400

    Text to Speech

---
+++ More_Content_After_The_Course/text_to_speech_from_file.py
@@ -0,0 +1,8 @@
+from gtts import gTTS
+import os
+with open("example.txt","r") as document:
+    myText = document.read()
+    language = "en"
+    output = gTTS(text=myText,lang=language, slow=False)
+    output.save("output.mp3")
+os.system("open output.mp3")
