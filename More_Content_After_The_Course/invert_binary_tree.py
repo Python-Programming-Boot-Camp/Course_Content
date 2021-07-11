acfd5f345fcd8d9ecd561aa59dbe0fe9a5fbc7aa
Jonathan Karr <jka83321@gmail.com>
2021-07-11 18:20:57 -0400

    invert binary tree

---
+++ More_Content_After_The_Course/invert_binary_tree.py
@@ -0,0 +1,40 @@
+class Node:
+   def __init__(self, data):
+      self.left = None
+      self.right = None
+      self.data = data
+
+   def insert(self, data):
+      if self.data:
+         if data < self.data:
+            if self.left is None:
+               self.left = Node(data)
+            else:
+               self.left.insert(data)
+         elif data > self.data:
+            if self.right is None:
+               self.right = Node(data)
+            else:
+               self.right.insert(data)
+         else:
+            self.data = data
+
+    
+def invert_binary_tree(tree):
+    if not tree:
+        return
+    left = tree.left
+    right = tree.right
+    tree.left = right
+    tree.right = left
+    invert_binary_tree(tree.left)
+    invert_binary_tree(tree.right)
+        
+
+root = Node(7)
+root.insert(4)
+root.insert(10)
+root.insert(3)
+root.insert(5)
+root.insert(8)
+root.insert(11)
