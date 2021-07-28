d29e210c41a90828a24581adf018be5040284c21
Jonathan Karr <jka83321@gmail.com>
2021-07-27 23:57:28 -0400

    packing and unpacking

---
+++ More_Content_After_The_Course/packinng_and_unpacking_dictionaries.py
@@ -0,0 +1,11 @@
+def pack_it(**kwargs):
+    print(kwargs)
+    print(kwargs['a'])
+    print(type(kwargs))
+pack_it(a = "animal", b="bear")
+
+def unpack_it (c,d):
+    print(c)
+    print(d)
+kwargs = {"c":"cat","d":"dog"}
+unpack_it(**kwargs)
